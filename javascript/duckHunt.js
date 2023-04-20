57b7e9c70c2e6adf835f16ba46fe8ba1edc42ab6
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-04-20 16:44:08 +0100

    Refactoring new create_levels

--- javascript/duckHunt.js
+++ javascript/duckHunt.js
@@ -1,23 +1,48 @@
-const maxLevels = 5;
-let currentLevel = 1;
-let gamePaused = true;
-let gameMuted = false;
-let gameFullscreen = false;
-let numberOfDucks;
 const ducks = () => {
   return document.querySelectorAll('.duck:not(.falling)');
 };
 const shots = document.querySelectorAll('#shot div');
 const dogs = document.querySelectorAll('.dog');
-
 const bullets = () => {
   return document.querySelectorAll('#shot .bullet:not(.lost)');
 };
+const dogLaugh = document.getElementById('dogLaugh');
+const maxLevels = 5;
+let currentLevel = 1;
+let gamePaused = true;
+let gameMuted = false;
+let gameFullscreen = false;
+let numberOfDucks = currentLevel + 1;
+
+dogLaugh.addEventListener('animationend', () => {
+  dogLaugh.style.display = 'none';
+});
+
+/*
+ducks().forEach(function (duck) {
+  duck.style.display = 'none';
+});
+*/
+
+function startGame() {
+  //const { left } = dogWalk.getBoundingClientRect();
+  document.getElementById('gameOver').style.display = 'none';
+  document.getElementById('start').style.display = 'none';
+  document.getElementById('dogWalk').classList.add('jump');
+  newGame();
+}
+
+function newGame() {
+  gamePaused = false;
+  createDucks(numberOfDucks);
+  createShotDivs(numberOfDucks * 2);
+  createHitDivs(numberOfDucks);
+  showDucks();
+}
 
 function createDucks(n) {
   const duckContainer = document.createElement('div');
   duckContainer.className = 'duck-container';
-
   for (let i = 1; i <= n; i++) {
     const duck = document.createElement('div');
     duck.className = 'duck';
@@ -52,35 +77,13 @@ function showDucks() {
   });
 }
 
-function startGame() {
-  document.getElementById('start').style.display = 'none';
-
-  // TODO Coordenada x de onde está o dog quando inicia o jogo;
-  const { left } = dogWalk.getBoundingClientRect();
-  document.getElementById('dogWalk').classList.add('jump');
-  newGame(2);
-}
-
-function newGame(n) {
-  currentLevel = n - 1;
-  document.getElementById('gameOver').style.display = 'none';
-  gamePaused = false;
-  numberOfDucks = n;
-  createDucks(numberOfDucks);
-  createShotDivs(numberOfDucks * 2);
-  createHitDivs(numberOfDucks);
-  showDucks();
-}
-
 window.onclick = function (e) {
   if (!gamePaused && e.target.tagName !== 'BUTTON') {
     decreaseBullet();
     if (e.target.classList == 'duck') {
       const fallingDuck = e.target;
-
-      // TODO Coordenadas de onde cai o passareco
-      const x = e.clientX;
-      const y = e.clientY;
+      //const x = e.clientX;
+      //const y = e.clientY;
       fallingDuck.classList.add('falling');
       document.getElementById('dogWithDuck').style.display = 'block';
       increaseScore();
@@ -95,9 +98,37 @@ window.onclick = function (e) {
   checkGameOver();
 };
 
+function decreaseBullet() {
+  const bulletDivs = document.querySelectorAll('#shot .bullet');
+  for (let i = bulletDivs.length; i > 0; i--) {
+    const shot = bulletDivs[i - 1];
+    if (!shot.classList.contains('lost')) {
+      shot.classList.add('lost');
+      break;
+    }
+  }
+}
+
+function increaseScore() {
+  const score = document.querySelector('#score').innerHTML;
+  const scoreHTML = document.querySelector('#score');
+  let count = Number(score);
+  scoreHTML.innerHTML = count + 200;
+}
+
+function hit() {
+  const hit = document.querySelector('#hit');
+  const divs = hit.querySelectorAll('div');
+  for (let i = 0; i < divs.length; i++) {
+    const div = divs[i];
+    if (!div.classList.contains('success')) {
+      div.classList.add('success');
+      break;
+    }
+  }
+}
+
 function checkGameOver() {
-  console.log(bullets().length);
-  console.log(ducks().length);
   if (bullets().length == 0 && ducks().length > 0) {
     document.getElementById('winner').innerHTML = "Yikes! You lost :(";
     document.getElementById('gameOver').style.display = 'block';
@@ -110,20 +141,8 @@ function checkGameOver() {
     } else {
       document.getElementById('winner').innerHTML = "You've won! NEXT LEVEL!";
       document.getElementById('gameOver').style.display = 'block';
-      document.querySelectorAll('.duck').forEach(duck => {
-        duck.classList.remove('flying');
-        duck.classList.remove('falling');
-      });
-      document.querySelectorAll('.duck').forEach(duck => {
-        duck.parentNode.removeChild(duck);
-      });
-      document.querySelectorAll('#shot div').forEach(shot => {
-        shot.parentNode.removeChild(shot);
-      });
-      document.querySelectorAll('#hit div').forEach(hit => {
-        hit.parentNode.removeChild(hit);
-      });
-      newGame(currentLevel + 2);
+      currentLevel++;
+      newLevel();
     }
   }
 }
@@ -144,7 +163,7 @@ function restart() {
   document.querySelectorAll('#hit div').forEach(hit => {
     hit.parentNode.removeChild(hit);
   });
-  clearScore();
+  document.querySelector('#score').innerHTML = 0;
   // TODO Refactor this in scss
   const dogWalk = document.getElementById('dogWalk');
   dogWalk.classList.remove('jump');
@@ -153,49 +172,22 @@ function restart() {
   parent.appendChild(dogWalk);
 }
 
-const dogLaugh = document.getElementById('dogLaugh');
-
-dogLaugh.addEventListener('animationend', () => {
-  dogLaugh.style.display = 'none';
-});
-
-const decreaseBullet = () => {
-  const bulletDivs = document.querySelectorAll('#shot .bullet');
-  for (let i = bulletDivs.length; i > 0; i--) {
-    const shot = bulletDivs[i - 1];
-    if (!shot.classList.contains('lost')) {
-      shot.classList.add('lost');
-      break;
-    }
-  }
-};
-
-const increaseScore = () => {
-  const score = document.querySelector('#score').innerHTML;
-  const scoreHTML = document.querySelector('#score');
-  let count = Number(score);
-  scoreHTML.innerHTML = count + 200;
-};
-
-function clearScore() {
-  document.querySelector('#score').innerHTML = 0;
+function newLevel() {
+  document.querySelectorAll('.duck').forEach(duck => {
+    duck.classList.remove('flying');
+    duck.classList.remove('falling');
+  });
+  document.querySelectorAll('.duck').forEach(duck => {
+    duck.parentNode.removeChild(duck);
+  });
+  document.querySelectorAll('#shot div').forEach(shot => {
+    shot.parentNode.removeChild(shot);
+  });
+  document.querySelectorAll('#hit div').forEach(hit => {
+    hit.parentNode.removeChild(hit);
+  });
+  newGame();
 }
-
-const hit = () => {
-  const hit = document.querySelector('#hit');
-  const divs = hit.querySelectorAll('div');
-  for (let i = 0; i < divs.length; i++) {
-    const div = divs[i];
-    if (!div.classList.contains('success')) {
-      div.classList.add('success');
-      break;
-    }
-  }
-};
-
-ducks().forEach(function (duck) {
-  duck.style.display = 'none';
-});
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'p') {
